8beed1ad4bb5c1a0eaa15853edb690f911f3789a
slotpagante <slotpagantebr@gmail.com>
2025-07-22 17:06:37 -0300

    Create sites.js

---
+++ sites.js
@@ -0,0 +1,11 @@
+const listaSites = [
+  {
+    "nome": "SlotMania",
+    "descricao": "Paga todo dia, saques rápidos e bônus diário. Testado e aprovado!",
+    "imagem": "https://via.placeholder.com/88x88?text=SlotMania",
+    "linkConvite": "https://slotmania.com/ref/seulink",
+    "avaliacao": 5,
+    "bonus": "Bônus de boas-vindas: 100% até R$500"
+  },
+  // ...outros sites
+];
